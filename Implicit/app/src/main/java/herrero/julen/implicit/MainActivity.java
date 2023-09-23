e4fed55152ed13a9f7e087baae5fdb0ffeb864f2
Julen-H <114407756+Julen-H@users.noreply.github.com>
2023-09-23 04:32:02 +0200

    Add coments 2023-09-23

--- Implicit/app/src/main/java/herrero/julen/implicit/MainActivity.java
+++ Implicit/app/src/main/java/herrero/julen/implicit/MainActivity.java
@@ -3,37 +3,38 @@ package herrero.julen.implicit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button bilatuLekua = findViewById(R.id.lekuButton);
-
         bilatuLekua.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 EditText lekuStr = findViewById(R.id.locName);
-                String herria = lekuStr.getText().toString();
+                String herria = lekuStr.getText().toString(); // lekuaren izena jaso
+                // Lekuaren izena maps-eko url-arekin konkatenatu
                 String urlOsoa = "http://maps.google.com/maps?q=" + herria;
 
-                Uri getLoc = Uri.parse(urlOsoa);
-                Intent intent = new Intent(Intent.ACTION_VIEW, getLoc);
+                Uri getLoc = Uri.parse(urlOsoa); // url-a sortu
+                Intent intent = new Intent(Intent.ACTION_VIEW, getLoc); // intent-a sortu
 
-                Log.d("URL_DEBUG", getLoc.toString());
+                Log.d("URL_DEBUG", getLoc.toString()); // url-aren debug-a
 
-                startActivity(intent);
+                startActivity(intent); // activity-a hasi
 
                 // Hau da errorea, ez du funtzionatzen bloke honekin. Zuzenean activitya hasi
                 /*if (intent.resolveActivity(getPackageManager()) != null) {
@@ -43,22 +44,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button partekatu = findViewById(R.id.shareButton);
-
         partekatu.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
 
                 EditText partekatuTxt = findViewById(R.id.shareTxt);
-                String testua = partekatuTxt.getText().toString();
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_TEXT, testua);
-                intent.setType("text/plain");
+                String testua = partekatuTxt.getText().toString(); // partekatuko den testua lortu
+
+                Intent intent = new Intent(); // intent berria sortu
+                intent.setAction(Intent.ACTION_SEND); // akzioa ezarri
+                intent.putExtra(Intent.EXTRA_TEXT, testua); // testua extra moduan ezarri
+                intent.setType("text/plain"); // tipoa ezarri
 
                 Intent prt = Intent.createChooser(intent, null);
-                startActivity(prt);
-
+                startActivity(prt); // activity-a hasi
             }
         });
     }
@@ -66,13 +65,13 @@ public class MainActivity extends AppCompatActivity {
 
         EditText webOrria = findViewById(R.id.urlStr);
         Button bilatuWb = findViewById(R.id.findUrl);
-        String weba = webOrria.getText().toString();
+        String weba = webOrria.getText().toString(); // web orriaren url-a lortu
 
-        Uri web = Uri.parse(weba);
-        Intent intent = new Intent(Intent.ACTION_VIEW, web);
+        Uri web = Uri.parse(weba); // url-a kargatu
+        Intent intent = new Intent(Intent.ACTION_VIEW, web); // intent-a sortu
 
         if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+            startActivity(intent); // activity-a hasi
         }
     }
 }
